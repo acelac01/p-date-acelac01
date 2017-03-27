f19ef060db73d916d215ef7ea21fe3f9d1fecfb8
Álvaro Celada <acelac01@estudiantes.unileon.es>
2017-03-27 13:44:07 +0200

    Modificaciones

---
+++ src/MainDate.java
@@ -0,0 +1,10 @@
+class MainDate
+{
+	public static void main(String args[])
+	{
+		Date today;
+		today = new Date (22,3,2017);
+		today.printMonthsSameAmountDate();
+		today.getNumsDayMonth();	
+	}
+}
